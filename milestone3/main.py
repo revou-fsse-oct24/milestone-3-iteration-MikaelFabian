9123ac1423f293331e6d5c2c6c219cc2aa6dbf45
MikaelFabian <mikaelfabian@gmail.com>
2025-04-08 20:19:04 +0700

    adding flask application

--- milestone3/main.py
+++ milestone3/main.py
@@ -1,6 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
 def main():
-    print("Hello from milestone3!")
+    return "Hello, World!"
+
+@app.route('/about')
+def about():
+    return "About Page"
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
